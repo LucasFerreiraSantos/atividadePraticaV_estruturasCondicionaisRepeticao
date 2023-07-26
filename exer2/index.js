47040a9d3c3bb9bb3528eca4c142f3370d0f51d3
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-26 20:14:52 -0300

    feito o exer2

--- exer2/index.js
+++ exer2/index.js
@@ -1,4 +1,20 @@
+let salarioMasculino = 0
+let salarioFeminino = 0
+let sexoMasculino = 0
+let sexoFeminino = 0
+do{
+  let escolha = prompt('Qual o sexo do funcionário?\nM(masculino) e\nF(feminino): ')
+    if(escolha == 'M' || escolha == 'm'){
+      salarioMasculino += parseFloat(prompt('Qual o seu salário atual?'))
+      sexoMasculino++
+    } else{
+      salarioFeminino += parseFloat(prompt('Qual o seu salário atual?'))
+      sexoFeminino++
+    }
+}while(confirm('Deseja continuar: '))
 
+document.write(`<P>O total de salários PAGOS AOS HOMENS é de: ${salarioMasculino.toFixed(2)}. De um total de ${sexoMasculino} funcionários.</P>`)
+document.write(`<P>O total de salários PAGOS ÀS MULHERES é de: ${salarioFeminino.toFixed(2)}. De um total de ${sexoFeminino} funcionárias.</P>`)
 
 // 2. Desenvolva um aplicativo que leia o salário e o sexo de vários
 // funcionários. No final, mostre o total de salários pagos aos homens e
