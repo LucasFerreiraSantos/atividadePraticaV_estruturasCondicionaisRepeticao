cba86528c3925107c3ad323b395694075e3c1791
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-27 19:52:25 -0300

    feito o exer5

--- exer5/index.js
+++ exer5/index.js
@@ -1,4 +1,14 @@
+const distanciaPercorrer = parseFloat(prompt('Qual a distância que você deseja percorrer? '));
 
+  if(distanciaPercorrer <= 200){
+    let TAXA = 0.50;
+    const valorTotal = distanciaPercorrer * TAXA;
+    document.write(`Foi cobrada uma TAXA por cada Km percorrido de R$${TAXA.toFixed(2)}. Você percorreu ${distanciaPercorrer}Km, portanto, o valor total de sua viagem é de R$${valorTotal.toFixed(2)}.`);
+  } else {
+    let TAXA = 0.45;
+    const valorTotal = distanciaPercorrer * TAXA;
+    document.write(`Foi cobrada uma TAXA por cada Km percorrido de R$${TAXA.toFixed(2)}. Você percorreu ${distanciaPercorrer}Km, portanto, o valor total de sua viagem é de R$${valorTotal.toFixed(2)}.`);
+  }
 
 // 5. Faça um algoritmo que pergunte a distância que um passageiro
 // deseja percorrer em Km. Calcule o preço da passagem, cobrando
