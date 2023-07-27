a0edf0ab5f894bfd04886a105355f96584f095ec
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-27 20:06:08 -0300

    feito o exer6 concluida as tarefas

--- exer6/index.js
+++ exer6/index.js
@@ -1,4 +1,14 @@
-
+const largura = parseInt(prompt('Qual a largura do terreno? '))
+const comprimento = parseInt(prompt('E seu comprimento? '))
+const areaTerreno = largura * comprimento
+document.write(`A área do terreno é de ${areaTerreno}m2.`)
+  if(areaTerreno < 100){
+    document.write('<p>TERRENO POPULAR!</p>')
+  } else if(areaTerreno >= 100 || areaTerreno <= 500){
+    document.write('<p>TERRENO MASTER</p>!')
+  }else{
+    document.write('<p>TERRENO VIP</p>!')
+  }
 
 // 6. Faça um programa que leia a largura e o comprimento de um
 // terreno retangular, calculando e mostrando a sua área em m2. O
